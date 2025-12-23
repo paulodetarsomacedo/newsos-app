3ba70eec443b8b61035ff35c7641a411b68eaea8
Paulo de Tarso Macedo <paulodetarso@MacBook-Pro-de-Paulo.local>
2025-12-22 23:57:35 -0300

    t50046

--- app/page.tsx
+++ app/page.tsx
@@ -2622,15 +2622,7 @@ function HappeningTab({ openArticle, openStory, isDarkMode, newsData, onRefresh,
             </div>
             <div>
                
-                {isContextLoading ? (
-                    <span className="text-xs font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-400 to-orange-400 animate-shimmer-text">
-                        Os maiores eventos, em múltiplos ângulos...
-                    </span>
-                ) : (
-                    <p className={`text-xs font-medium ${isDarkMode ? 'text-zinc-400' : 'text-zinc-500'}`}>
-                        
-                    </p>
-                )}
+                
             </div>
         </div>
 
