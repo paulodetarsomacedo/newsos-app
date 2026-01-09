d60ea3bf360cc3c72f2d9d9e0084bad5f8047d07
Paulo de Tarso Macedo <paulodetarso@MacBookPro.lan>
2026-01-09 20:40:49 -0300

    ia11019

--- app/page.tsx
+++ app/page.tsx
@@ -1133,21 +1133,21 @@ const NewsCard = React.memo(({ news, isSelected, isRead, isSaved, isLiked, isDar
                 {/* LOGO SOLTO */}
                 <img 
                     src={news.logo} 
-                    className="relative z-10 w-10 h-10 rounded-lg bg-white p-0.5 shadow-lg border border-white/40" 
+                    className="relative z-10 -mt-0.5 w-12 h-12 rounded-lg bg-white p-0.5 shadow-lg border border-white/40" 
                     onError={(e) => e.target.style.display = 'none'} 
                 />
                 {/* PÍLULA APENAS NO NOME */}
-                <div className="-ml-3 pl-2 pr-2 py-2  bg-black/20 backdrop-blur-md px-1 rounded-r-md border border-white/10 shadow-lg flex items-center justify-center h-fit">
+                <div className="-ml-3 pl-5   bg-black/20 backdrop-blur-md px-1 rounded-r-md border border-white/10 shadow-lg flex items-center justify-center h-fit">
                     <span className="text-[10px] font-black text-white uppercase tracking-wider">{news.source}</span>
                 </div>
-                <div className="ml-2 bg-black/20 backdrop-blur-md px-2 rounded-md border border-white/5">
+                <div className="ml-2 bg-black/20 backdrop-blur-md px-1 rounded-md border border-white/5">
                     <span className="text-[10px] font-bold text-white">{displayTime}</span>
                 </div>
             </div>
 
             {/* Botões no Canto Superior Direito */}
             <div className="absolute top-4 right-4 z-20">
-                <div className="flex flex-col items-center gap-3 p-2 rounded-full bg-black/20 backdrop-blur-md border border-white/10">
+                <div className="flex flex-col items-center gap-3 p-2 rounded-full bg-black/30 backdrop-blur-md border border-white/10">
                     <button onClick={(e) => { e.stopPropagation(); if(onToggleLike) onToggleLike(news); }} className={`p-1.5 transition-colors ${isLiked ? 'text-rose-500' : 'text-white/80 hover:text-white'}`}>
                         <Heart size={22} fill={isLiked ? "currentColor" : "none"} />
                     </button>
