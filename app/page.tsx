cbb23f0252e4f1c9430777ce6b3e10105db2a29a
Paulo de Tarso Macedo <paulodetarso@MacBookPro.lan>
2026-01-09 21:11:42 -0300

    ia110257

--- app/page.tsx
+++ app/page.tsx
@@ -1140,7 +1140,7 @@ const NewsCard = React.memo(({ news, isSelected, isRead, isSaved, isLiked, isDar
                 <div className="-ml-3 pl-4 pr-2 py-2  bg-black/50  px-1 rounded-r-md border border-white/10 shadow-lg flex items-center justify-center h-fit">
                     <span className="text-[10px] font-black text-white uppercase tracking-wider">{news.source}</span>
                 </div>
-                <div className="ml-2 bg-black/50  py-1.5 px-1.5 rounded-md border border-white/5">
+                <div className="ml-2 bg-black/50  py-1 px-2 rounded-md border border-white/5">
                     <span className="text-[12px] font-bold text-white">{displayTime}</span>
                 </div>
             </div>
